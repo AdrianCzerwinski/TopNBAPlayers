ee39437abca8c88ce73896a762ed4296889ad411
Adrian Czerwiński <adrczer@gmail.com>
2022-02-18 15:59:20 +0100

    "Final before Testing"

--- app/src/main/java/com/adrianczerwinski/borutoapp/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/adrianczerwinski/borutoapp/presentation/screens/search/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.adrianczerwinski.borutoapp.presentation.screens.search
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,6 +9,8 @@ import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
 import com.adrianczerwinski.borutoapp.presentation.common.ListContent
+import com.adrianczerwinski.borutoapp.ui.theme.statusBarColor
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @ExperimentalCoilApi
 @Composable
@@ -15,6 +18,10 @@ fun SearchScreen(
     navController: NavHostController,
     searchViewModel: SearchViewModel = hiltViewModel()
 ) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(
+        color = MaterialTheme.colors.statusBarColor
+    )
 
     val searchQuery by searchViewModel.searchQuery
     val heroes = searchViewModel.searchedHeroes.collectAsLazyPagingItems()
